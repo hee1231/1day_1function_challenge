aa85583cdefc399895bbfff5ca804b5e9ffbb289
hee1231 <102498600+hee1231@users.noreply.github.com>
2022-06-01 01:17:00 +0900

    Add 1일 1함수 외우기

---
+++ Day10_0531.js
@@ -0,0 +1,18 @@
+// 2022.05.31
+// 10진수를 2진수로 변환하기 (제코배 43번)
+// 160 -> 10100000
+
+// 방법 1
+let dec = 160;
+// dec(10진수 숫자)를 2로 나눈 나머지값을 순차적으로 저장할 배열 생성
+let arrBinary = [];
+
+while (dec) {
+  arrBinary.push(dec % 2);
+  dec = Math.floor(dec / 2);
+}
+
+console.log(arrBinary.reverse().join(""));
+
+// 방법 2
+console.log(dec.toString(2));
