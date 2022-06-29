1537e6954905124fb7cf9454acdc55c80754d32b
hee1231 <102498600+hee1231@users.noreply.github.com>
2022-06-29 17:45:34 +0900

    Add 1일 1함수 외우기

---
+++ Day19_0629.js
@@ -0,0 +1,16 @@
+// 2022.06.29
+// 콜백함수(2) : 익명 함수 사용하기
+
+// 함수를 선언합니다.
+
+function callThreeTimes (callback) {
+  for (let i = 0; i < 3; i++) {
+    callback(i);
+  }
+}
+
+// 함수를 호출합니다.
+
+callThreeTimes (function (i) {
+  console.log(`${i}번째 함수 호출`);
+});
